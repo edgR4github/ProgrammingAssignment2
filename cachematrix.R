b0e35103b31f7b5c76bd656750bb70e799791631
edgR4github <lomobrother@yahoo.com>
2016-06-16 18:24:10 -0500

    Week 3 assignment dawg!

--- cachematrix.R
+++ cachematrix.R
@@ -5,11 +5,58 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+z <- dim(x)  #Check dimension of inputted values
+ 
+  #if matrix not square quit function
+  if( z[1] != z[2])  
+    {
+    message("Inputted value not a square matrix")
+    message("Function will not execute")
+    return()
+    }
+  
+  I <- NULL  #initialize I (used to hold inverse)
+  
+  #set <- function(y)   #Set data...not sure why this is needed
+  #  {
+  #  x <<- y
+  #  I <<- NULL
+  #  }
+  
+  get <- function()   #get data
+    {
+    x
+    }
+  
+  setinv <- function(inv) #set inverse I
+    {
+    I <<- inv
+    }
+  
+  getinv <- function() #Returns inverse I
+    {
+    I 
+    }
+  
+  list ( get = get, setinv = setinv, getinv = getinv)
+
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+         I <- x$getinv() #Get inverse if it exist
+  
+  #  If I not NULL, use cached I
+  if( !is.null(I))
+    {
+    message("getting cached inverse")
+    return(I)
+    }
+  
+  data <- x$get()  #  Get Data
+  I <- solve(data,...)  # compute inverse
+  x$setinv(I)  # Set inverse in cache
+  
 }
